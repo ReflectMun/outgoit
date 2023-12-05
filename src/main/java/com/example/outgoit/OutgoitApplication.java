036047548a783faf8635d645193815dad437eefc
ButterFlakes <wksh229@proton.me>
2023-12-05 16:13:32 +0900

    webapp, WEB-INF 폴더 추가

---
+++ src/main/java/com/example/outgoit/OutgoitApplication.java
@@ -0,0 +1,13 @@
+package com.example.outgoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutgoitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutgoitApplication.class, args);
+    }
+
+}
