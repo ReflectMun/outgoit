e34359c9965e07adcd1d09d544e2e6ad203de18a
ButterFlakes <wksh229@proton.me>
2023-12-05 18:18:27 +0900

    그냥 수정함

--- src/main/java/com/example/outgoit/camp/CampingSearchService.java
+++ src/main/java/com/example/outgoit/camp/CampingSearchService.java
@@ -7,6 +7,6 @@ public class CampingSearchService {
     private final String baseURL = "https://apis.data.go.kr/B551011/GoCamping/searchList";
     private final String key = "s7uOVVVt02w8vbG4QdP35gAmWK%2F9GkwQ0GIX7lLHcQ6wXeuPyOF1LJia95iYlnTpaA2IJk6uYQekBwKXpqDspw%3D%3D";
     public String GetSearchedCampingAreaList(String campName) {
-        return "D";
+        return "A";
     }
 }
