51e034038e48d7eb3b498d75fdb8c8e1f2b61e66
ButterFlakes <wksh229@proton.me>
2023-12-05 18:12:56 +0900

    baseURL, key 추가

--- src/main/java/com/example/outgoit/camp/CampingSearchService.java
+++ src/main/java/com/example/outgoit/camp/CampingSearchService.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CampingSearchService {
+    private final String baseURL = "https://apis.data.go.kr/B551011/GoCamping/searchList";
+    private final String key = "s7uOVVVt02w8vbG4QdP35gAmWK%2F9GkwQ0GIX7lLHcQ6wXeuPyOF1LJia95iYlnTpaA2IJk6uYQekBwKXpqDspw%3D%3D";
+
     public String temp(){
         return "A";
     }
