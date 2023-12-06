6a7168a3051e8442cf960cb397b777e2b04a1352
ms <kms05189@naver.com>
2023-12-06 17:59:04 +0900

    캠핑장에 대한 DTO, Controller , Service 세팅완료 , 등산로에 대한 TrailRouteDTO 만들어만 둠

--- src/main/java/com/example/outgoit/camp/CampingSearchService.java
+++ src/main/java/com/example/outgoit/camp/CampingSearchService.java
@@ -1,12 +1,47 @@
 package com.example.outgoit.camp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class CampingSearchService {
-    private final String baseURL = "https://apis.data.go.kr/B551011/GoCamping/searchList";
+    //    고캠핑  API 주소 => "https://apis.data.go.kr/B551011/GoCamping/searchList"
     private final String key = "s7uOVVVt02w8vbG4QdP35gAmWK%2F9GkwQ0GIX7lLHcQ6wXeuPyOF1LJia95iYlnTpaA2IJk6uYQekBwKXpqDspw%3D%3D";
-    public String GetSearchedCampingAreaList(String campName) {
-        return "A";
+
+    public ArrayList<CampingAreaInfoDTO> GetSearchedCampingAreaList(String campName) {
+
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host("apis.data.go.kr")
+                .path("/B551011/GoCamping/searchList")
+                .queryParam("MobileOS", "etc")
+                .queryParam("MobileApp", "outgoit")
+                .queryParam("_type", "json")
+                .queryParam("serviceKey", key)
+                .queryParam("keyword", URLEncoder.encode(campName))
+                .build(true);
+
+//        uriComponents.toUriString();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            URL url = new URL(uriComponents.toUriString());
+            ApiResponse res = mapper.readValue(url, ApiResponse.class);
+            ArrayList<CampingAreaInfoDTO> list =
+                    new ArrayList<>(Arrays.asList(res.getResponse().getBody().getItems().getItem()));
+            System.out.println(list.get(0).getMapY() + ", " + list.get(0).getMapX());
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("캠핑장 검색 에러");
+            return null;
+        }
     }
 }
---
+++ src/main/java/com/example/outgoit/trail/TrailRouteDTO.java
@@ -0,0 +1,14 @@
+package com.example.outgoit.trail;
+
+public class TrailRouteDTO {
+    private String status;
+}
+
+class PropertiesData{
+    private int sec_len; // 등산로의 총 거리 단위(m)
+    private int up_min; // 상행시간(산 올라가는데 걸리는 시간) 단위(분)
+    private  String cat_nam; // 등산로 난이도 ex) 상, 중, 하
+    private  String mntn_nm; // 등산로 이름 ex) 설악산 대청봉
+
+
+}
