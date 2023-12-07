1e7465aa4e9b245bd2960ccfcbeecc52e86b8095
ButterFlakes <wksh229@proton.me>
2023-12-07 16:02:21 +0900

    등산로 api 작성 완료, 캠핑장 리뷰 작성 시작

---
+++ src/main/java/com/example/outgoit/review/CampingReviewDTO.java
@@ -0,0 +1,54 @@
+package com.example.outgoit.review;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Entity(name = "CampingReview")
+@Getter
+@Setter
+@NoArgsConstructor
+public class CampingReviewDTO { // 이 DTO는 캠핑장 리뷰에 관한 데이터를 담을 DTO임
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "comment_number", nullable = false, unique = true)
+    private Long commentNumber; // 리뷰 고유번호
+
+    @Column(nullable = false)
+    private String nickname; // 작성자 닉네임
+
+    @Column(nullable = false)
+    private String password; // 작성 시 입력한 비밀번호
+
+    @Column(name = "review_content", nullable = false)
+    private String reviewContent; // 리뷰 내용
+
+    @Column(nullable = false)
+    private Integer rating; // 평점(별점)
+
+    @Column(name = "commented_date", nullable = false)
+    private LocalDate commentedDate; // 리뷰를 작성한 날짜
+
+    @PrePersist
+    public void prePersist(){
+        this.commentedDate = LocalDate.now(ZoneId.of("Asia/Seoul"));
+    }
+
+    public CampingReviewDTO(
+            String nickname,
+            String password,
+            String reviewContent,
+            Integer rating
+    ){
+        this.nickname = nickname;
+        this.password = password;
+        this.reviewContent = reviewContent;
+        this.rating = rating;
+    }
+}
