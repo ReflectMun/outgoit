31f81f2bc0d854366f4c46e8c52e05bf66f41927
orchid_k <kuran1286@gmail.com>
2023-12-12 18:20:26 +0900

    [rh] camping 파일 전체 수정, 메모 추가 시 부모 영역 div 같이 안 내려가던 것 수정, css 파일 수정, js 기능 입력

---
+++ src/main/resources/static/rh/js/camping.js
@@ -0,0 +1,111 @@
+$(function () {
+  $("#icon1").show();
+  $("#icon2").show();
+  $("#icon3").show();
+  $("#icon4").show();
+  $("#icon5").show();
+  $("#icon6").show();
+  $("#icon7").show();
+  $("#icon8").show();
+  $("#icon9").show();
+  $("#content1").hide();
+  $("#content2").hide();
+  $("#content3").hide();
+  $("#content4").hide();
+  $("#content5").hide();
+  $("#content6").hide();
+  $("#content7").hide();
+  $("#content8").hide();
+  $("#content9").hide();
+
+  //1번
+  $("#icon1").click(function () {
+    $("#icon1").hide();
+    $("#content1").show();
+  });
+  $("#content1").click(function () {
+    $("#content1").hide();
+    $("#icon1").show();
+  });
+
+  //2번
+
+  $("#icon2").click(function () {
+    $("#icon2").hide();
+    $("#content2").show();
+  });
+  $("#content2").click(function () {
+    $("#content2").hide();
+    $("#icon2").show();
+  });
+
+  //3번
+  $("#icon3").click(function () {
+    $("#icon3").hide();
+    $("#content3").show();
+  });
+  $("#content3").click(function () {
+    $("#content3").hide();
+    $("#icon3").show();
+  });
+
+  //4번
+  $("#icon4").click(function () {
+    $("#icon4").hide();
+    $("#content4").show();
+  });
+  $("#content4").click(function () {
+    $("#content4").hide();
+    $("#icon4").show();
+  });
+
+  //5번
+  $("#icon5").click(function () {
+    $("#icon5").hide();
+    $("#content5").show();
+  });
+  $("#content5").click(function () {
+    $("#content5").hide();
+    $("#icon5").show();
+  });
+
+  //6번
+  $("#icon6").click(function () {
+    $("#icon6").hide();
+    $("#content6").show();
+  });
+  $("#content6").click(function () {
+    $("#content6").hide();
+    $("#icon6").show();
+  });
+
+  //7번
+  $("#icon7").click(function () {
+    $("#icon7").hide();
+    $("#content7").show();
+  });
+  $("#content7").click(function () {
+    $("#content7").hide();
+    $("#icon7").show();
+  });
+
+  //8번
+  $("#icon8").click(function () {
+    $("#icon8").hide();
+    $("#content8").show();
+  });
+  $("#content8").click(function () {
+    $("#content8").hide();
+    $("#icon8").show();
+  });
+
+  //9번
+  $("#icon9").click(function () {
+    $("#icon9").hide();
+    $("#content9").show();
+  });
+  $("#content9").click(function () {
+    $("#content9").hide();
+    $("#icon9").show();
+  });
+});
