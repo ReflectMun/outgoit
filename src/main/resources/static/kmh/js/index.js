fd262b1d101ddfd6479ba140d8c29be558b66d00
minhyeee <alsgp01238664@gmail.com>
2023-12-15 14:56:41 +0900

    [KMH] 인덱스 자잘한 효과 수정, 호버

---
+++ src/main/resources/static/kmh/js/index.js
@@ -0,0 +1,26 @@
+const txt = document.getElementById('txt1');
+txt.addEventListener('mouseover',function () {
+    txt.style.color = 'rgb(222, 111, 26)';
+})
+
+txt.addEventListener('mouseout',function () {
+    txt.style.color = '';
+})
+
+const txt2 = document.getElementById('txt2');
+txt2.addEventListener('mouseover',function () {
+    txt2.style.color = 'rgb(222, 111, 26)';
+})
+
+txt2.addEventListener('mouseout',function () {
+    txt2.style.color = '';
+})
+
+const txt3 = document.getElementById('txt3');
+txt3.addEventListener('mouseover',function () {
+    txt3.style.color = 'rgb(222, 111, 26)';
+})
+
+txt3.addEventListener('mouseout',function () {
+    txt3.style.color = '';
+})
