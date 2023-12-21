155a8833278796534ac18dc37088c29b2c8403f4
minhyeee <alsgp01238664@gmail.com>
2023-12-21 15:46:01 +0900

    [KMH] 일단 커미이이잇

--- src/main/java/com/example/outgoit/trail/TrailRoutePageController.java
+++ src/main/java/com/example/outgoit/trail/TrailRoutePageController.java
@@ -2,11 +2,20 @@ package com.example.outgoit.trail;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/trail")
 public class TrailRoutePageController {
+    private final TrailRouteService trailRouteService;
+
+    public TrailRoutePageController(TrailRouteService trailRouteService){
+        this.trailRouteService = trailRouteService;
+    }
     @GetMapping
     public String getTrailRoutePage(){
         return "jiho/trail";
@@ -16,8 +25,5 @@ public class TrailRoutePageController {
         return "kmh/hiking";
     }
 
-    @GetMapping("/draft2min")
-    public String sendDraft2(){
-        return "kmh/hiking3";
-    }
+
 }
