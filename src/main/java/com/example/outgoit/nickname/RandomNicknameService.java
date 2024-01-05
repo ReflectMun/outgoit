19fd15b1bc0a01cf2b44b457bfdf11ef7ad2d2a5
orchid_k <kuran1286@gmail.com>
2024-01-05 11:47:07 +0900

    [rh] 뭔지 몰으겠어요

--- src/main/java/com/example/outgoit/nickname/RandomNicknameService.java
+++ src/main/java/com/example/outgoit/nickname/RandomNicknameService.java
@@ -4,19 +4,23 @@ package com.example.outgoit.nickname;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.Reader;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class RandomNicknameService {
     public static void main(String[] args) {
-        ObjectMapper objectMapper = new ObjectMapper();
+//        ObjectMapper objectMapper = new ObjectMapper();
         try {
             String filePath = "src/main/resources/static/rh/js/nickname.json";
-            RandomNicknameDTO nickname = objectMapper.readValue(new File(filePath), RandomNicknameDTO.class);
-            System.out.println(nickname.getDeterminers());
-            System.out.println(nickname.getAnimals());
-
-            ArrayList<String> determinersList = determinersList.get()
+//            RandomNicknameDTO nickname = objectMapper.readValue(new File(filePath), RandomNicknameDTO.class);
+//            System.out.println(nickname.getDeterminers());
+//            System.out.println(nickname.getAnimals());
+//            Random random = new Random();
+            String nickname = new String(Files.readAllBytes(Paths.get(filePath).getFileName()))
 
 
         } catch (Exception e) {
