cbf8403579ef189a55d3731a0efed70633cf86e6
ButterFlakes <wksh229@proton.me>
2024-01-05 09:54:21 +0900

    [jiho] 지역날시 리스트 컨트롤러에서 서비스로 옮김

--- src/main/java/com/example/outgoit/weather/WeatherService.java
+++ src/main/java/com/example/outgoit/weather/WeatherService.java
@@ -12,10 +12,36 @@ import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Service
 public class WeatherService {
     private final String key = "s7uOVVVt02w8vbG4QdP35gAmWK%2F9GkwQ0GIX7lLHcQ6wXeuPyOF1LJia95iYlnTpaA2IJk6uYQekBwKXpqDspw%3D%3D";
+
+    public HashMap<String, WeatherApiResponseDTO> getWeatherDataList(){
+        HashMap<String, WeatherApiResponseDTO> result = new HashMap();
+
+        result.put("서울", getWeatherDataByGrid(60, 127));
+        result.put("부산", getWeatherDataByGrid(98, 76));
+        result.put("대구", getWeatherDataByGrid(89, 90));
+        result.put("인천", getWeatherDataByGrid(55, 124));
+        result.put("광주", getWeatherDataByGrid(58, 74));
+        result.put("대전", getWeatherDataByGrid(67, 100));
+        result.put("울산", getWeatherDataByGrid(102, 84));
+        result.put("세종", getWeatherDataByGrid(66, 103));
+        result.put("수원", getWeatherDataByGrid(60, 121));
+        result.put("춘천", getWeatherDataByGrid(73, 134));
+        result.put("강릉", getWeatherDataByGrid(92, 131));
+        result.put("청주", getWeatherDataByGrid(69, 106));
+        result.put("전주", getWeatherDataByGrid(63, 89));
+        result.put("목포", getWeatherDataByGrid(50, 67));
+        result.put("안동", getWeatherDataByGrid(91, 106));
+        result.put("진주", getWeatherDataByGrid(81, 75));
+        result.put("제주", getWeatherDataByGrid(52, 38));
+
+        return result;
+    }
+
     public WeatherApiResponseDTO getWeatherDataByCoordinate(Double lati, Double lngi){
         GridCoordinate coord = toGridCoordinate(lati, lngi);
 
